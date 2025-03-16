b984fc0e8851e241787a68dd6fb3c36d0be19d33
MorningStar1314 <104242332+MorningStar1314@users.noreply.github.com>
2025-03-17 01:36:41 +0800

    Update 1140316

--- main.c
+++ main.c
@@ -14,13 +14,59 @@ void initStack(Stack *s){
 	s->top = -1;
 }
 
+int isEmpty(Stack *s){
+	return s->top == -1;
+}
+
 void push(Stack *s, int value){
-	s->top++;
-	s->item[s->top] = value;
+	if (s->top < MAX - 1){
+		s->top++;
+		s->item[s->top] = value;
+	}
 }
 
 int pop(Stack *s){
-	return s->item[s->top--];
+	if (!isEmpty(s)){
+		return s->item[s->top--];
+	}
+}
+
+int peek(Stack *s){
+	if (!isEmpty(s)){
+		return s->item[s->top];
+	}
+}
+
+int operatorPriorty(char op){
+	if (op == '+' || op == '-'){
+		return 1;
+	}
+	else if (op == '*' || op == '/'){
+		return 2;
+	}
+}
+
+int Operation(int a, int b, char op){
+	if (op == '+') return a + b;
+	if (op == '-') return a - b;
+	if (op == '*') return a * b;
+	if (op == '/'){
+		if (b != 0){
+			return a / b;
+		}
+		else{
+			return 0;
+		}
+	}
+}
+
+void process(Stack *numbers, Stack *operators){
+	int a, b;
+	char op;
+	b = pop(numbers);
+	a = pop(numbers);
+	op = pop(operators);
+	push(numbers, Operation(a, b, op));
 }
 
 int parseNumber(char *ch, int *i){
@@ -32,35 +78,37 @@ int parseNumber(char *ch, int *i){
 	return num;
 }
 
+int evaluate(char *ch){
+	Stack numbers, operators;
+	int i = 0;
+	initStack(&numbers);
+	initStack(&operators);
+
+	while (ch[i] != '\0'){
+		if (isdigit(ch[i])){
+			push(&numbers, parseNumber(ch, &i));
+		}
+		else if (ch[i] == '('){
+			push(&operators, ch[i]);
+			i++;
+		}
+	}
+
+	
+
+}
+
 
 
 int main() {
 	
-	Stack numbers;
-	Stack operators;
-	
-	initStack(&numbers);
-	initStack(&operators);
-	
+	int result = 0;
 	char ch[MAX];
 	printf("Enter a formula:");
 	scanf("%s", ch);
 	
-	int i = 0;
-	while (ch[i] != '\0' && ch[i] != '\n'){
-		if(isdigit(ch[i])){
-			int num = parseNumber(ch, &i);
-			push(&numbers, num);
-		}
-		
-		else if (ch[i] == '+' || ch[i] == '-' || ch[i] == '*' || ch[i] == '/'){
-			push(&operators, ch[i]);
-			i++;
-		}
-		
-		i++;
-	}
-	
+	result = evaluate(ch);
+	printf("Result: %d", result);
 	
 	return 0;
 }
